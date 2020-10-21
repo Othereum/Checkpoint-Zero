51138fa2a53f8cc3102a97be06fdb2c635711b9f
Othereum <seokjin.dev@gmail.com>
2020-10-22 00:13:49 +0900

    TODO comment

--- Source/CP0/Private/CP0CharacterMovement.cpp
+++ Source/CP0/Private/CP0CharacterMovement.cpp
@@ -3,6 +3,8 @@
 #include "CP0CharacterMovement.h"
 #include "BindInputAction.h"
 
+// TODO: 이 코드는 아예 서버에서 실행되는 게 나을 것 같다
+// BindInputAction을 Character로 다시 옮기고, 이 코드가 서버에서 호출되도록 만들자
 struct FSprintAction
 {
     void Enable(UCP0CharacterMovement* Movement) const
